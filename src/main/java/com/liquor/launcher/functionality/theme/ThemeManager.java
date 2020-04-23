b3b6c604e8b5062e0f28f90fc3859f41cb06e9ee
Yasin <yasin_programmer@hotmail.com>
2020-04-23 13:02:59 +0200

    changed one time execution to periodically

--- src/main/java/com/liquor/launcher/functionality/theme/ThemeManager.java
+++ src/main/java/com/liquor/launcher/functionality/theme/ThemeManager.java
@@ -2,4 +2,15 @@ package com.liquor.launcher.functionality.theme;
 
 public class ThemeManager {
 
+    private static ThemeManager instance;
+
+    private Theme currentTheme;
+
+    public Theme getCurrentTheme() {
+        return currentTheme;
+    }
+
+    public static ThemeManager getInstance() {
+        return instance == null ? instance = new ThemeManager() : instance;
+    }
 }
